015f2c5139df120603c1fd06337f40e83254dc31
MaJoApol <mariajoanaceron@gmail.com>
2024-05-08 14:44:24 -0300

    firstCom

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print("hello bruno")
+from flask import Flask
+from database.db import db 
 
-print("hello bruno")
 
-print("hello dgfdfgdfgdfgdfg")
+
+class MyServer(): #classe que inicializa e guarda o funcionamento do flask
+    def __init__(self) -> None:
+        self.app = Flask(__name__) #obj de flask que estamos importando
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/apiflask' #configurações especificas do ambiente que vamos usar
+
+        db.init_app(self.app) #inicializa o flask para usar com a extensão (mysqalchemy )
+
+    def run(self):
+        return self.app.run(port=3000, debug=True, host='localhost')
+
+if __name__ == '__main__' :
+    app = MyServer() #instanciando ne aff
+    app.run() #inicializando
