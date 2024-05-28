5b92c02f8ebddc860e05248b733661392fd1307e
brunoCGhisi <brunocostaghisi@gmail.com>
2024-05-28 19:04:14 -0300

    imagem menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Button, Container, Box  } from '@mui/material';
+import menuImage from './assets/libraryMenu.jpg';
 
 export const App = () => {
   return (
     <div className="App">
+
       <Box component="section" 
-      display= 'flex'
-      justifyContent= 'center'
-      p= '2'
-      sx={{border: '1px solid grey'}}>
-      
-        <Button variant="contained" color='primary'> Hello world </Button>
-        <Button variant="contained"> Hello world^^2 </Button>
+      sx={{
+        border: '1px solid grey',
+        display: 'flex',
+        justifyContent: 'center',
+        p: '2'
+      }}>
+        
+        <Button variant="contained" color='error'> Criar </Button>
       </Box>
+
     </div>
+
   );
+
 }
 
 export default App;
