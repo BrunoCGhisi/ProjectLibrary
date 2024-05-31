3bf9f2799432c6b08369cfcb57cfceb9958f4333
brunoCGhisi <brunocostaghisi@gmail.com>
2024-05-31 14:43:17 -0300

    pfvr node modules nao vem junto

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import { Button, Container, Box  } from '@mui/material';
-import menuImage from './assets/libraryMenu.jpg';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { CategoryVO } from './services/types'
+import {getCategories} from './services/libraryService'
 
-export const App = () => {
+function App() {
+  //UseState esta recebendo o tipo CategoryVO, pois vamos utilziar ele
+  const [categories, setCategories] = useState<CategoryVO[]>([]) 
+
+  async function findCategories() {
+    const response = await getCategories()
+    setCategories(response)
+  }
+
+  //toda vez que entrar no página, vai a função getCategories, nesse caso vai pegar os clientes
+  useEffect(() => {
+    findCategories(); //retorna uma lista de categorias
+  }, []);             //dependecias (controla a execução do useEffect), então nesse caso TODA vez que CATEGORIES for alterado, vai executar novamente
+
   return (
-    <div className="App">
-
-      <Box component="section" 
-      sx={{
-        border: '1px solid grey',
-        display: 'flex',
-        justifyContent: 'center',
-        p: '2'
-      }}>
-        
-        <Button variant="contained" color='error'> Criar </Button>
-      </Box>
-
+    <div>
+      {categories.map((categoria) => (
+        <div
+          key={categoria.id_categoria}
+          style={{
+            display: 'flex',
+            border: '1px solid #ff0',
+            flexDirection: 'column',
+            gap: '10px',
+            alignItems: 'center'
+          }}
+        > 
+        <span> Id_Categoria {categoria.id_categoria}  </span>
+        <span> Categoria    {categoria.categoria}     </span>
+        </div>
+      )
+    
+    )}
     </div>
-
-  );
-
+  )
 }
 
-export default App;
 
-
+export default App
