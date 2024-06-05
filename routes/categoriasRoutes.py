b35209d6897b6dbf503c35d784f4f81651adbe76
MaJoApol <mariajoanaceron@gmail.com>
2024-06-05 16:35:21 -0300

    crud_autor_workando

--- routes/categoriasRoutes.py
+++ routes/categoriasRoutes.py
@@ -2,4 +2,4 @@ from controllers.categoriasController import categoriasController
 
 
 def categoriasRoutes(app):
-    app.route('/categoria/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'])(categoriasController)
+    app.route('/categoria', methods=['GET', 'POST', 'PUT', 'DELETE'])(categoriasController)
